a0dffb6f0e281e1226cead28f81f082f70649f13
Damian <damianionut990@gmail.com>
2020-11-13 12:20:03 +0200

    Styled bottom nav bar

--- app/src/main/java/com/example/bookarest/activity_home.java
+++ app/src/main/java/com/example/bookarest/activity_home.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationMenu;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -17,6 +19,8 @@ public class activity_home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_home);
 
 
