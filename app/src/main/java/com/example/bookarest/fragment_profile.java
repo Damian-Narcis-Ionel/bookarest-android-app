49ec27c6e7196b36f1e818f8d5204902bf30b32f
Damian <damianionut990@gmail.com>
2020-11-21 17:11:37 +0200

    finilized profile fragment, started my books fragment

--- app/src/main/java/com/example/bookarest/fragment_profile.java
+++ app/src/main/java/com/example/bookarest/fragment_profile.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class fragment_profile extends Fragment {
 
@@ -15,6 +17,19 @@ public class fragment_profile extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        initializeSpinner(rootView);
+
+        return rootView;
+
+
+    }
+
+    private void initializeSpinner(View view){
+        Spinner spinner =(Spinner)view.findViewById(R.id.spinner_profile_favorite_genres);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(),R.array.spinner_profile_favorite_genres, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
