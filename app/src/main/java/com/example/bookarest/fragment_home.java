f9c9ea77f2246b9d5dd03d1f813efc5416ab0a4a
Damian <damianionut990@gmail.com>
2020-11-22 13:05:22 +0200

    startedd home fragment

--- app/src/main/java/com/example/bookarest/fragment_home.java
+++ app/src/main/java/com/example/bookarest/fragment_home.java
@@ -1,5 +1,6 @@
 package com.example.bookarest;
 
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 public class fragment_home extends Fragment {
 
+    ImageView img;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        img = v.findViewById(R.id.img_home_currently_reading);
+        img.setClipToOutline(true);
+
+        return v;
     }
 }
